cfc67548005f61ac62b6ac3c84be7f206a36d755
User1 <User1@LP-F9CR8R1.HCLT.CORP.HCL.IN>
2019-04-05 12:21:30 +0530

    Initial Draft

---
+++ Trading/src/main/java/com/rmisecurity/trade/TradingApplication.java
@@ -0,0 +1,14 @@
+package com.rmisecurity.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+@SpringBootConfiguration
+public class TradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApplication.class, args);
+	}
+
+}
